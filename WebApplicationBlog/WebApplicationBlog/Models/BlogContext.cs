82bc836a4cddc10db54f06d4ae823b28354f1e5b
Shabbab Haider <shancoraed@outlook.com>
2020-08-04 14:14:34 -0500

    Added Blog Application

---
+++ WebApplicationBlog/WebApplicationBlog/Models/BlogContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace WebApplicationBlog.Models
+{
+    public class BlogContext : DbContext
+    {
+
+        public DbSet<BlogPost> blogPost { get; set; }
+
+        public BlogContext(DbContextOptions<BlogContext> options) : base(options)
+            {
+
+
+
+            }
+
+
+
+
+    }
+}
---
+++ WebApplicationBlog/WebApplicationBlog/Models/BlogPost.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplicationBlog.Models
+{
+    public class BlogPost
+    {
+
+        [Key]
+        public int Id { get; set; }
+        
+        public string blogTitle { get; set; }
+
+        public string content { get; set; }
+
+        public DateTime blogDate{ get; set; }
+
+
+
+
+    }
+}
