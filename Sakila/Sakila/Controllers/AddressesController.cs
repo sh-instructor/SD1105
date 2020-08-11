b3b07e7b378cdaca612eee621659189dde8a1ce3
Shabbab Haider <shancoraed@outlook.com>
2020-08-11 08:40:14 -0500

    Lesson 3 - SD1105

---
+++ Sakila/Sakila/Controllers/AddressesController.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Sakila.Models;
+
+namespace Sakila.Controllers
+{
+    public class AddressesController : Controller
+    {
+        private readonly SakilaContext _context;
+
+        public AddressesController(SakilaContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Addresses
+        public async Task<IActionResult> Index()
+        {
+            var sakilaContext = _context.Address.Include(a => a.City);
+            return View(await sakilaContext.ToListAsync());
+        }
+
+        // GET: Addresses/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var address = await _context.Address
+                .Include(a => a.City)
+                .FirstOrDefaultAsync(m => m.AddressId == id);
+            if (address == null)
+            {
+                return NotFound();
+            }
+
+            return View(address);
+        }
+
+        // GET: Addresses/Create
+        public IActionResult Create()
+        {
+            ViewData["CityId"] = new SelectList(_context.City, "CityId", "City1");
+            return View();
+        }
+
+        // POST: Addresses/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("AddressId,Address1,Address2,District,CityId,PostalCode,Phone,LastUpdate")] Address address)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(address);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CityId"] = new SelectList(_context.City, "CityId", "City1", address.CityId);
+            return View(address);
+        }
+
+        // GET: Addresses/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var address = await _context.Address.FindAsync(id);
+            if (address == null)
+            {
+                return NotFound();
+            }
+            ViewData["CityId"] = new SelectList(_context.City, "CityId", "City1", address.CityId);
+            return View(address);
+        }
+
+        // POST: Addresses/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("AddressId,Address1,Address2,District,CityId,PostalCode,Phone,LastUpdate")] Address address)
+        {
+            if (id != address.AddressId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(address);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!AddressExists(address.AddressId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CityId"] = new SelectList(_context.City, "CityId", "City1", address.CityId);
+            return View(address);
+        }
+
+        // GET: Addresses/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var address = await _context.Address
+                .Include(a => a.City)
+                .FirstOrDefaultAsync(m => m.AddressId == id);
+            if (address == null)
+            {
+                return NotFound();
+            }
+
+            return View(address);
+        }
+
+        // POST: Addresses/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var address = await _context.Address.FindAsync(id);
+            _context.Address.Remove(address);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool AddressExists(int id)
+        {
+            return _context.Address.Any(e => e.AddressId == id);
+        }
+    }
+}
---
+++ Sakila/Sakila/Models/CustomerAuditLog.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Sakila.Models
+{
+    public partial class CustomerAuditLog
+    {
+        public int CustomerAuditId { get; set; }
+        public int? CustomerId { get; set; }
+        public int? StoreId { get; set; }
+        public int? StoreIdOld { get; set; }
+        public string FirstName { get; set; }
+        public string FirstNameOld { get; set; }
+        public string LastName { get; set; }
+        public string LastNameOld { get; set; }
+        public string Email { get; set; }
+        public string EmailOld { get; set; }
+        public string Active { get; set; }
+        public string ActiveOld { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        public string UpdatedBy { get; set; }
+    }
+}
